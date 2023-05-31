867705bf778d847910e5b427f6c1dfa79e32c242
Vorleak Yek <vorleakyek@gmail.com>
2023-05-31 03:48:48 +0000

    Complete all tasks for feature 2.

--- js/main.js
+++ js/main.js
@@ -5,9 +5,20 @@ const title = document.querySelector('#title');
 const notes = document.querySelector('#notes');
 const form = document.querySelector('#form');
 const ul = document.querySelector('.list-entries');
+const entriesLink = document.querySelector('.enties-link');
+const newButton = document.querySelector('#btn-new-entry');
+const entryForm = document.querySelector('[data-view="entry-form"]');
+const entryPage = document.querySelector('[data-view="entries"]');
 
 photoUrl.addEventListener('input', photoPreview);
 button.addEventListener('click', submitForm);
+entriesLink.addEventListener('click', function (event) {
+  viewSwap('entries');
+});
+newButton.addEventListener('click', function (event) {
+  viewSwap('entry-form');
+});
+document.addEventListener('DOMContentLoaded', loadAllEntries);
 
 function photoPreview(event) {
   const inputUrl = event.target.value;
@@ -34,11 +45,18 @@ function submitForm(event) {
   data.entries.push(dataObj);
   imgPreview.src = 'images/placeholder-image-square.jpg';
   form.reset();
+
+  ul.prepend(renderEntry(dataObj));
+  viewSwap('entries');
+
+  if (data.entries.length === 1) {
+    toggleNoEntries();
+  }
 }
 
 function renderEntry(entry) {
   const newLi = document.createElement('li');
-  ul.append(newLi);
+  ul.prepend(newLi);
   const newDiv1 = document.createElement('div');
   newDiv1.setAttribute('class', 'row');
   newLi.append(newDiv1);
@@ -51,15 +69,16 @@ function renderEntry(entry) {
   const newDiv3 = document.createElement('div');
   newDiv3.className = 'column-full column-half';
   newDiv1.append(newDiv3);
-  const newh3 = document.createElement('h3');
-  newh3.textContent = entry.title;
-  newDiv3.append(newh3);
+  const newh2 = document.createElement('h2');
+  newh2.textContent = entry.title;
+  newh2.classList.add('no-margin');
+  newDiv3.append(newh2);
   const newpara = document.createElement('p');
   newpara.textContent = entry.notes;
+  newpara.classList.add('pre-wrap');
   newDiv3.append(newpara);
+  return newLi;
 }
-
-document.addEventListener('DOMContentLoaded', loadAllEntries);
 
 function loadAllEntries(event) {
   if (data.entries.length === 0) {
@@ -73,5 +92,28 @@ function loadAllEntries(event) {
 
   for (let i = 0; i < data.entries.length; i++) {
     renderEntry(data.entries[i]);
+  }
+
+  viewSwap(data.view);
+
+  if (data.entries === 0) {
+    toggleNoEntries();
+  }
+}
+
+function toggleNoEntries() {
+  const noData = document.querySelector('.no-data');
+  noData.classList.toggle('hidden');
+}
+
+function viewSwap(dataView) {
+  if (dataView === 'entry-form') {
+    entryForm.classList.remove('hidden');
+    entryPage.setAttribute('class', 'hidden');
+    data.view = dataView;
+  } else if (dataView === 'entries') {
+    entryPage.classList.remove('hidden');
+    entryForm.setAttribute('class', 'hidden');
+    data.view = dataView;
   }
 }
